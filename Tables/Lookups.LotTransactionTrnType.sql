7431eceaa7d15cad653b3a8adbefa908c179c5c2
Beardy <chris.johnson85@gmail.com>
2016-02-11 09:24:31 +0000

    Added primary key

--- Tables/Lookups.LotTransactionTrnType.sql
+++ Tables/Lookups.LotTransactionTrnType.sql
@@ -1,6 +1,8 @@
 CREATE TABLE [Lookups].[LotTransactionTrnType]
 (
-[TrnType] [char] (1) COLLATE Latin1_General_BIN NULL,
+[TrnType] [char] (1) COLLATE Latin1_General_BIN NOT NULL,
 [TrnTypeDescription] [varchar] (100) COLLATE Latin1_General_BIN NULL
 ) ON [PRIMARY]
+ALTER TABLE [Lookups].[LotTransactionTrnType] ADD 
+CONSTRAINT [LTTrnType] PRIMARY KEY CLUSTERED  ([TrnType]) ON [PRIMARY]
 GO
