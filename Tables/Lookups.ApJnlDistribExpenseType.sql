97152582c0cf6478c118abd7593ba0d66895f483
Beardy <chris.johnson85@gmail.com>
2016-02-16 16:10:25 +0000

    Added primary key

--- Tables/Lookups.ApJnlDistribExpenseType.sql
+++ Tables/Lookups.ApJnlDistribExpenseType.sql
@@ -1,6 +1,8 @@
 CREATE TABLE [Lookups].[ApJnlDistribExpenseType]
 (
-[ExpenseType] [varchar] (3) COLLATE Latin1_General_BIN NULL,
+[ExpenseType] [varchar] (3) COLLATE Latin1_General_BIN NOT NULL,
 [ExpenseTypeDesc] [varchar] (150) COLLATE Latin1_General_BIN NULL
 ) ON [PRIMARY]
+ALTER TABLE [Lookups].[ApJnlDistribExpenseType] ADD 
+CONSTRAINT [ExpenseTypeKey] PRIMARY KEY CLUSTERED  ([ExpenseType]) ON [PRIMARY]
 GO
