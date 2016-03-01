f16e89c5a95a1707a7af921b89dd698475c592dd
Beardy <chris.johnson85@gmail.com>
2016-03-01 13:38:46 +0000

    corrected link

--- Stored Procedures/Process.UspUpdate_GLAccountType.sql
+++ Stored Procedures/Process.UspUpdate_GLAccountType.sql
@@ -1,11 +1,12 @@
+
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
 CREATE Proc [Process].[UspUpdate_GLAccountType]
     (
-      @PrevCheck INT --if count is less than previous don't update
-    , @HoursBetweenUpdates INT
+      @PrevCheck Int --if count is less than previous don't update
+    , @HoursBetweenUpdates Int
     )
 As --exec  [Process].[UspUpdate_GLAccountType] 0,-1
     Begin
@@ -31,102 +32,87 @@ As --exec  [Process].[UspUpdate_GLAccountType] 0,-1
 
         Print 1;
 --check if table exists and create if it doesn't
-        If ( Not Exists ( Select
-                            *
-                          From
-                            INFORMATION_SCHEMA.TABLES
-                          Where
-                            TABLE_SCHEMA = 'Lookups'
-                            And TABLE_NAME = 'GLAccountType' )
+        If ( Not Exists ( Select    *
+                          From      [INFORMATION_SCHEMA].[TABLES]
+                          Where     [TABLE_SCHEMA] = 'Lookups'
+                                    And [TABLE_NAME] = 'GLAccountType' )
            )
             Begin
                 Print 2;
                 Create --drop --alter 
-				Table Lookups.GLAccountType
+				Table [Lookups].[GLAccountType]
                     (
-                      GLAccountType VARCHAR(10)
-                    , GLAccountTypeDesc Varchar(250)
-                    , LastUpdated DATETIME2
+                      [GLAccountType] Varchar(10)
+                    , [GLAccountTypeDesc] Varchar(250)
+                    , [LastUpdated] DateTime2
                     );
             End;
 
 
 --check last time run and update if it's been longer than @HoursBetweenUpdates hours
-        Declare @LastDate DATETIME2;
+        Declare @LastDate DateTime2;
 
         Print 3;
-        Select
-            @LastDate = MAX(LastUpdated)
-        From
-            Lookups.GLAccountType;
+        Select  @LastDate = Max([LastUpdated])
+        From    [Lookups].[GLAccountType];
 
         If @LastDate Is Null
-            Or DATEDIFF(Hour, @LastDate, GETDATE()) > @HoursBetweenUpdates
+            Or DateDiff(Hour , @LastDate , GetDate()) > @HoursBetweenUpdates
             Begin
 	--Set time of run
-                Declare @LastUpdated DATETIME2;
-                Select
-                    @LastUpdated = GETDATE();
+                Declare @LastUpdated DateTime2;
+                Select  @LastUpdated = GetDate();
                 Print 4;
 	--create master list of how codes affect stock
                 Create --drop --alter 
-	Table #GLAccountType
+	Table [#GLAccountType]
                     (
-                      AccountType VARCHAR(10) Collate Latin1_General_BIN
-                    , AccountTypeDescription VARCHAR(250)
+                      [AccountType] Varchar(10) Collate Latin1_General_BIN
+                    , [AccountTypeDescription] Varchar(250)
                     );
 
                 Print 5;
-                Insert  #GLAccountType
-                        ( AccountType
-                        , AccountTypeDescription
+                Insert  [#GLAccountType]
+                        ( [AccountType]
+                        , [AccountTypeDescription]
                         )
-                        Select
-                            AccountType
-                          , AccountTypeDescription
-                        From
-                            (
-                              Select
-                                AccountType = 'A'
-                              , AccountTypeDescription = 'Asset'
-                              Union
-                              Select
-                                AccountType = 'C'
-                              , AccountTypeDescription = 'Capital'
-                              Union
-                              Select
-                                AccountType = 'E'
-                              , AccountTypeDescription = 'Expense'
-                              Union
-                              Select
-                                AccountType = 'L'
-                              , AccountTypeDescription = 'Liability'
-                              Union
-                              Select
-                                AccountType = 'R'
-                              , AccountTypeDescription = 'Revenue'
-                              Union
-                              Select
-                                AccountType = 'S'
-                              , AccountTypeDescription = 'Statistical'
-                              Union
-                              Select
-                                AccountType = 'T'
-                              , AccountTypeDescription = 'Template'
-                            ) t;
+                        Select  [t].[AccountType]
+                              , [t].[AccountTypeDescription]
+                        From    ( Select    [AccountType] = 'A'
+                                          , [AccountTypeDescription] = 'Asset'
+                                  Union
+                                  Select    [AccountType] = 'C'
+                                          , [AccountTypeDescription] = 'Capital'
+                                  Union
+                                  Select    [AccountType] = 'E'
+                                          , [AccountTypeDescription] = 'Expense'
+                                  Union
+                                  Select    [AccountType] = 'L'
+                                          , [AccountTypeDescription] = 'Liability'
+                                  Union
+                                  Select    [AccountType] = 'R'
+                                          , [AccountTypeDescription] = 'Revenue'
+                                  Union
+                                  Select    [AccountType] = 'S'
+                                          , [AccountTypeDescription] = 'Statistical'
+                                  Union
+                                  Select    [AccountType] = 'T'
+                                          , [AccountTypeDescription] = 'Template'
+                                ) [t];
 
 
 					
 	--all companies process the same way
-                Select
-                    [AccountType]= Coalesce(GAT.[AccountType],GM.AccountType)
-                  , [AccountTypeDescription] = COALESCE([AccountTypeDescription], 'Unknown')
+                Select  [AccountType] = Coalesce([GAT].[AccountType] ,
+                                                 [GM].[AccountType])
+                      , [AccountTypeDescription] = Coalesce([GAT].[AccountTypeDescription] ,
+                                                            'Unknown')
                 Into
                     #ResultsAccountTypeName 
-                From
-                    #GLAccountType As GAT
-					Full Outer Join SysproCompany40.dbo.GenMaster As GM
-					On GM.AccountType = GAT.AccountType;
+                From    [#GLAccountType] As [GAT]
+                        Full Outer Join [SysproCompany40].[dbo].[GenMaster] As [GM] On [GM].[AccountType] = [GAT].[AccountType]
+                Group By Coalesce([GAT].[AccountType] , [GM].[AccountType])
+                      , Coalesce([AccountTypeDescription] , 'Unknown');
 
 
 	--placeholder for anomalous results that are different to master list
@@ -135,73 +121,58 @@ As --exec  [Process].[UspUpdate_GLAccountType] 0,-1
 	--Where CompanyName = ''
 	--	And TrnType = '';
 
-                Insert  [Lookups].GLAccountType
+                Insert  [Lookups].[GLAccountType]
                         ( [GLAccountType]
                         , [GLAccountTypeDesc]
                         , [LastUpdated]
 	                    )
-                        Select
-                            [rcn].[AccountType]
-                          , [rcn].[AccountTypeDescription]
-                          , @LastUpdated
-                        From
-                            #ResultsAccountTypeName As [rcn];
+                        Select  [rcn].[AccountType]
+                              , [rcn].[AccountTypeDescription]
+                              , @LastUpdated
+                        From    [#ResultsAccountTypeName] As [rcn];
 
                 If @PrevCheck = 1
                     Begin
-                        Declare
-                            @CurrentCount INT
-                          , @PreviousCount INT;
+                        Declare @CurrentCount Int
+                          , @PreviousCount Int;
 	
-                        Select
-                            @CurrentCount = COUNT(*)
-                        From
-                            Lookups.[GLAccountType] As [cn]
-                        Where
-                            LastUpdated = @LastUpdated;
+                        Select  @CurrentCount = Count(*)
+                        From    [Lookups].[GLAccountType] As [cn]
+                        Where   [cn].[LastUpdated] = @LastUpdated;
 
-                        Select
-                            @PreviousCount = COUNT(*)
-                        From
-                            Lookups.[GLAccountType] As [cn]
-                        Where
-                            LastUpdated <> @LastDate;
+                        Select  @PreviousCount = Count(*)
+                        From    [Lookups].[GLAccountType] As [cn]
+                        Where   [cn].[LastUpdated] <> @LastDate;
 	
                         If @PreviousCount > @CurrentCount
                             Begin
-                                Delete
-                                    Lookups.[GLAccountType]
-                                Where
-                                    LastUpdated = @LastDate;
+                                Delete  [Lookups].[GLAccountType]
+                                Where   [LastUpdated] = @LastDate;
                                 Print 'UspUpdate_GLAccountType - Count has gone down since last run, no update applied';
                                 Print 'Current Count = '
-                                    + CAST(@CurrentCount As VARCHAR(5))
+                                    + Cast(@CurrentCount As Varchar(5))
                                     + ' Previous Count = '
-                                    + CAST(@PreviousCount As VARCHAR(5));
+                                    + Cast(@PreviousCount As Varchar(5));
                             End;
                         If @PreviousCount <= @CurrentCount
                             Begin
-                                Delete
-                                    Lookups.[GLAccountType]
-                                Where
-                                    LastUpdated <> @LastUpdated
-                                    Or [LastUpdated] Is Null;
+                                Delete  [Lookups].[GLAccountType]
+                                Where   [LastUpdated] <> @LastUpdated
+                                        Or [LastUpdated] Is Null;
                                 Print 'UspUpdate_GLAccountType - Update applied successfully';
                             End;
                     End;
                 If @PrevCheck = 0
                     Begin
-                        Delete
-                            Lookups.[GLAccountType]
-                        Where
-                            LastUpdated <> @LastUpdated;
+                        Delete  [Lookups].[GLAccountType]
+                        Where   [LastUpdated] <> @LastUpdated;
                         Print 'UspUpdate_GLAccountType - Update applied successfully';
                     End;
             End;
     End;
-    If DATEDIFF(Hour, @LastDate, GETDATE()) <= @HoursBetweenUpdates
+    If DateDiff(Hour , @LastDate , GetDate()) <= @HoursBetweenUpdates
         Begin
             Print 'UspUpdate_GLAccountType - Table was last updated at '
-                + CAST(@LastDate As VARCHAR(255)) + ' no update applied';
+                + Cast(@LastDate As Varchar(255)) + ' no update applied';
         End;
 GO
