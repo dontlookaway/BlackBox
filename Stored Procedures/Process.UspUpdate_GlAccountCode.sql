c5d559a4479cb6fe37b7b071f52cb3b0c47c28c5
Beardy <chris.johnson85@gmail.com>
2016-03-21 14:19:52 +0000

    updated formatting

--- Stored Procedures/Process.UspUpdate_GlAccountCode.sql
+++ Stored Procedures/Process.UspUpdate_GlAccountCode.sql
@@ -4,79 +4,77 @@ GO
 SET ANSI_NULLS ON
 GO
 CREATE Proc [Process].[UspUpdate_GlAccountCode]
-(@PrevCheck INT --if count is less than previous don't update
-,@HoursBetweenUpdates int
-)
+    (
+      @PrevCheck Int --if count is less than previous don't update
+    , @HoursBetweenUpdates Int
+    )
 As
-Begin
+    Begin
 /*
-Stored procedure created by Chris Johnson, Prometic Group September 2015 to populate table with amounts relating to	Purchase Order Status details
+Stored procedure created by Chris Johnson, Prometic Group September 2015 to populate table with amounts relating to	GL Account Code
 */
 
-Set NoCount On
+        Set NoCount On;
 
 
 --check if table exists and create if it doesn't
-If ( Not Exists ( Select
-                    *
-                  From
-                    INFORMATION_SCHEMA.TABLES
-                  Where
-                    TABLE_SCHEMA = 'Lookups'
-                    And TABLE_NAME = 'GlAccountCode' )
-   )
-    Begin
-        Create Table Lookups.GlAccountCode
-            (
-              Company VARCHAR(150)
-            , GlAccountCode CHAR(5)
-            , GlAccountDescription VARCHAR(150)
-            , LastUpdated DATETIME2
-            );
-    End;
+        If ( Not Exists ( Select    1
+                          From      [INFORMATION_SCHEMA].[TABLES]
+                          Where     [TABLE_SCHEMA] = 'Lookups'
+                                    And [TABLE_NAME] = 'GlAccountCode' )
+           )
+            Begin
+                Create Table [Lookups].[GlAccountCode]
+                    (
+                      [Company] Varchar(150)
+                    , [GlAccountCode] Char(5)
+                    , [GlAccountDescription] Varchar(150)
+                    , [LastUpdated] DateTime2
+                    );
+            End;
 
 
 --check last time run and update if it's been longer than @HoursBetweenUpdates hours
-Declare @LastDate DATETIME2
+        Declare @LastDate DateTime2;
 
-Select @LastDate=MAX(LastUpdated)
-From Lookups.GlAccountCode
+        Select  @LastDate = Max([LastUpdated])
+        From    [Lookups].[GlAccountCode];
 
-If @LastDate Is Null Or DATEDIFF(Hour,@LastDate,GETDATE())>@HoursBetweenUpdates
-Begin
+        If @LastDate Is Null
+            Or DateDiff(Hour , @LastDate , GetDate()) > @HoursBetweenUpdates
+            Begin
 	--Set time of run
-	Declare @LastUpdated DATETIME2; Select @LastUpdated=GETDATE();
+                Declare @LastUpdated DateTime2;
+                Select  @LastUpdated = GetDate();
 
 	--create master list of how codes affect stock
-	Create Table #OrdersGlAccountCode
-		(
-		  GlAccountCode VARCHAR(5)
-		, GlAccountDescription VARCHAR(150)
-		);
+                Create Table [#OrdersGlAccountCode]
+                    (
+                      [GlAccountCode] Varchar(5)
+                    , [GlAccountDescription] Varchar(150)
+                    );
 
-	Insert  #OrdersGlAccountCode
-	        ( GlAccountCode
-	        , GlAccountDescription
-	        )
-			Select
-				GlAccountCode
-			  , GlAccountDescription
-			From
-				(
-				  Select GlAccountCode = 'M', GlAccountDescription='Merchandise expense'
-				  Union
-				  Select GlAccountCode = 'F', GlAccountDescription='Freight-in expense'
-				  Union
-				  Select GlAccountCode = 'O', GlAccountDescription='Other expense'
-				) t;
+                Insert  [#OrdersGlAccountCode]
+                        ( [GlAccountCode]
+                        , [GlAccountDescription]
+	                    )
+                        Select  [t].[GlAccountCode]
+                              , [t].[GlAccountDescription]
+                        From    ( Select    [GlAccountCode] = 'M'
+                                          , [GlAccountDescription] = 'Merchandise expense'
+                                  Union
+                                  Select    [GlAccountCode] = 'F'
+                                          , [GlAccountDescription] = 'Freight-in expense'
+                                  Union
+                                  Select    [GlAccountCode] = 'O'
+                                          , [GlAccountDescription] = 'Other expense'
+                                ) [t];
 
 	--create script to pull data from each db into the tables
-	Declare @SQL VARCHAR(Max) = '
+                Declare @SQL Varchar(Max) = '
 		USE [?];
 		Declare @DB varchar(150),@DBCode varchar(150)
-		Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end'
-		+ --Only query DBs beginning SysProCompany
-		'
+		Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end
 		IF left(@DB,13)=''SysproCompany'' and right(@DB,3)<>''SRS''
 		BEGIN				
 		Insert #Table1
@@ -84,74 +82,66 @@ Begin
 		Select @DBCode
 		End';
 
-	--execute script against each db, populating the base tables
-	--Exec [Process].[ExecForEachDB] @cmd =    @SQL;
-
 	--all companies process the same way
-	Select
-		CompanyName='40'
-	  , O.GlAccountCode
-	  , O.GlAccountDescription
-	Into
-		#ResultsGlAccountCode
-	From
-		 #OrdersGlAccountCode O
-		
+                Select  [CompanyName] = '40'
+                      , [O].[GlAccountCode]
+                      , [O].[GlAccountDescription]
+                Into    [#ResultsGlAccountCode]
+                From    [#OrdersGlAccountCode] [O];
 
-	--placeholder for anomalous results that are different to master list
-	--Update #ResultsGlAccountCode
-	--Set amountmodifier = 0--Set amount
-	--Where CompanyName = ''
-	--	And TrnType = '';
+                Insert  [Lookups].[GlAccountCode]
+                        ( [Company]
+                        , [GlAccountCode]
+                        , [GlAccountDescription]
+                        , [LastUpdated]
+	                    )
+                        Select  [CompanyName]
+                              , [GlAccountCode]
+                              , [GlAccountDescription]
+                              , @LastUpdated
+                        From    [#ResultsGlAccountCode];
 
-	Insert  Lookups.GlAccountCode
-	        ( Company
-	        , GlAccountCode
-	        , GlAccountDescription
-	        , LastUpdated
-	        )
-			Select
-				CompanyName
-			  , GlAccountCode
-			  , GlAccountDescription
-			  , @LastUpdated
-			From
-				#ResultsGlAccountCode;
-
-	If @PrevCheck=1
-	Begin
-		Declare @CurrentCount INT, @PreviousCount INT
+                If @PrevCheck = 1
+                    Begin
+                        Declare @CurrentCount Int
+                          , @PreviousCount Int;
 	
-		Select @CurrentCount=COUNT(*) From Lookups.GlAccountCode
-		Where LastUpdated=@LastUpdated
+                        Select  @CurrentCount = Count(*)
+                        From    [Lookups].[GlAccountCode]
+                        Where   [LastUpdated] = @LastUpdated;
 
-		SELECT @PreviousCount=COUNT(*) From Lookups.GlAccountCode
-		Where LastUpdated<>@LastUpdated
+                        Select  @PreviousCount = Count(*)
+                        From    [Lookups].[GlAccountCode]
+                        Where   [LastUpdated] <> @LastUpdated;
 	
-		If @PreviousCount>@CurrentCount
-			Begin
-				Delete Lookups.GlAccountCode
-				Where LastUpdated=@LastUpdated
-				Print 'UspUpdate_GlAccountCode - Count has gone down since last run, no update applied'
-				Print 'Current Count = '+CAST(@CurrentCount As VARCHAR(5))+' Previous Count = '+CAST(@PreviousCount As VARCHAR(5))
-			End
-		If @PreviousCount<=@CurrentCount
-			Begin
-				Delete Lookups.GlAccountCode
-				Where LastUpdated<>@LastUpdated
-				Print 'UspUpdate_GlAccountCode - Update applied successfully'
-			End
-	end
-	If @PrevCheck=0
-		Begin
-			Delete Lookups.GlAccountCode
-			Where LastUpdated<>@LastUpdated
-			Print 'UspUpdate_GlAccountCode - Update applied successfully'
-		End
-	End
-End
-If DATEDIFF(Hour,@LastDate,GETDATE())<=@HoursBetweenUpdates
-Begin
-	Print 'UspUpdate_GlAccountCode - Table was last updated at '+CAST(@LastDate As VARCHAR(255))+' no update applied'
-End
+                        If @PreviousCount > @CurrentCount
+                            Begin
+                                Delete  [Lookups].[GlAccountCode]
+                                Where   [LastUpdated] = @LastUpdated;
+                                Print 'UspUpdate_GlAccountCode - Count has gone down since last run, no update applied';
+                                Print 'Current Count = '
+                                    + Cast(@CurrentCount As Varchar(5))
+                                    + ' Previous Count = '
+                                    + Cast(@PreviousCount As Varchar(5));
+                            End;
+                        If @PreviousCount <= @CurrentCount
+                            Begin
+                                Delete  [Lookups].[GlAccountCode]
+                                Where   [LastUpdated] <> @LastUpdated;
+                                Print 'UspUpdate_GlAccountCode - Update applied successfully';
+                            End;
+                    End;
+                If @PrevCheck = 0
+                    Begin
+                        Delete  [Lookups].[GlAccountCode]
+                        Where   [LastUpdated] <> @LastUpdated;
+                        Print 'UspUpdate_GlAccountCode - Update applied successfully';
+                    End;
+            End;
+    End;
+    If DateDiff(Hour , @LastDate , GetDate()) <= @HoursBetweenUpdates
+        Begin
+            Print 'UspUpdate_GlAccountCode - Table was last updated at '
+                + Cast(@LastDate As Varchar(255)) + ' no update applied';
+        End;
 GO
--- Stored Procedures/Process.UspUpdate_GlExpenseCode.sql
+++ Stored Procedures/Process.UspUpdate_GlExpenseCode.sql
@@ -4,80 +4,78 @@ GO
 SET ANSI_NULLS ON
 GO
 CREATE Proc [Process].[UspUpdate_GlExpenseCode]
-(@PrevCheck INT --if count is less than previous don't update
-,@HoursBetweenUpdates int
-)
+    (
+      @PrevCheck Int --if count is less than previous don't update
+    , @HoursBetweenUpdates Int
+    )
 As
-Begin
+    Begin
 /*
 Stored procedure created by Chris Johnson, Prometic Group September 2015 to populate table with amounts relating to	Purchase Order Status details
 */
 
-Set NoCount On
+        Set NoCount On;
 
 
 --check if table exists and create if it doesn't
-If ( Not Exists ( Select
-                    *
-                  From
-                    INFORMATION_SCHEMA.TABLES
-                  Where
-                    TABLE_SCHEMA = 'Lookups'
-                    And TABLE_NAME = 'GlExpenseCode' )
-   )
-    Begin
-        Create Table Lookups.GlExpenseCode
-            (
-              Company VARCHAR(150)
-            , GlExpenseCode CHAR(5)
-            , GlExpenseDescription VARCHAR(150)
-            , LastUpdated DATETIME2
-            );
-    End;
+        If ( Not Exists ( Select    1
+                          From      [INFORMATION_SCHEMA].[TABLES]
+                          Where     [TABLE_SCHEMA] = 'Lookups'
+                                    And [TABLE_NAME] = 'GlExpenseCode' )
+           )
+            Begin
+                Create Table [Lookups].[GlExpenseCode]
+                    (
+                      [Company] Varchar(150)
+                    , [GlExpenseCode] Char(5)
+                    , [GlExpenseDescription] Varchar(150)
+                    , [LastUpdated] DateTime2
+                    );
+            End;
 
 
 --check last time run and update if it's been longer than @HoursBetweenUpdates hours
-Declare @LastDate DATETIME2
+        Declare @LastDate DateTime2;
 
-Select @LastDate=MAX(LastUpdated)
-From Lookups.GlExpenseCode
+        Select  @LastDate = Max([LastUpdated])
+        From    [Lookups].[GlExpenseCode];
 
-If @LastDate Is Null Or DATEDIFF(Hour,@LastDate,GETDATE())>@HoursBetweenUpdates
-Begin
+        If @LastDate Is Null
+            Or DateDiff(Hour , @LastDate , GetDate()) > @HoursBetweenUpdates
+            Begin
 	--Set time of run
-	Declare @LastUpdated DATETIME2; Select @LastUpdated=GETDATE();
+                Declare @LastUpdated DateTime2;
+                    Select  @LastUpdated = GetDate();
 
 	--create master list of how codes affect stock
-	Create Table #OrdersGlExpenseCode
-		(
-		  GlExpenseCode VARCHAR(5)
-		, GlExpenseDescription VARCHAR(150)
-		);
+                Create Table [#OrdersGlExpenseCode]
+                    (
+                      [GlExpenseCode] Varchar(5)
+                    , [GlExpenseDescription] Varchar(150)
+                    );
 
-	Insert  #OrdersGlExpenseCode
-	        ( GlExpenseCode
-	        , GlExpenseDescription
-	        )
-			Select
-				GlExpenseCode
-			  , GlExpenseDescription
-			From
-				(
-				  Select GlExpenseCode = 'M', GlExpenseDescription='Merchandise expense'
-				  Union
-				  Select GlExpenseCode = 'F', GlExpenseDescription='Freight-in expense'
-				  Union
-				  Select GlExpenseCode = 'O', GlExpenseDescription='Other expense'
-				) t;
+                Insert  [#OrdersGlExpenseCode]
+                        ( [GlExpenseCode]
+                        , [GlExpenseDescription]
+	                    )
+                        Select  [t].[GlExpenseCode]
+                              , [t].[GlExpenseDescription]
+                        From    ( Select    [GlExpenseCode] = 'M'
+                                          , [GlExpenseDescription] = 'Merchandise expense'
+                                  Union
+                                  Select    [GlExpenseCode] = 'F'
+                                          , [GlExpenseDescription] = 'Freight-in expense'
+                                  Union
+                                  Select    [GlExpenseCode] = 'O'
+                                          , [GlExpenseDescription] = 'Other expense'
+                                ) [t];
 
 	
 	--create script to pull data from each db into the tables
-	Declare @SQL VARCHAR(Max) = '
+                Declare @SQL Varchar(Max) = '
 		USE [?];
 		Declare @DB varchar(150),@DBCode varchar(150)
-		Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end'
-		+ --Only query DBs beginning SysProCompany
-		'
+		Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end
 		IF left(@DB,13)=''SysproCompany'' and right(@DB,3)<>''SRS''
 		BEGIN				
 		Insert #Table1
@@ -85,70 +83,69 @@ Begin
 		Select @DBCode
 		End';
 
-	--execute script against each db, populating the base tables
-	--Exec [Process].[ExecForEachDB] @cmd =    @SQL;
-
 	--all companies process the same way
-	Select
-		CompanyName='40'
-	  , O.GlExpenseCode
-	  , O.GlExpenseDescription
-	Into
-		#ResultsGlExpenseCode
-	From
-		 #OrdersGlExpenseCode O
+                Select  [CompanyName] = '40'
+                      , [O].[GlExpenseCode]
+                      , [O].[GlExpenseDescription]
+                Into    [#ResultsGlExpenseCode]
+                From    [#OrdersGlExpenseCode] [O];
 		
 
 	--placeholder for anomalous results that are different to master list
 
-	Insert  Lookups.GlExpenseCode
-	        ( Company
-	        , GlExpenseCode
-	        , GlExpenseDescription
-	        , LastUpdated
-	        )
-			Select
-				CompanyName
-			  , GlExpenseCode
-			  , GlExpenseDescription
-			  , @LastUpdated
-			From
-				#ResultsGlExpenseCode;
+                Insert  [Lookups].[GlExpenseCode]
+                        ( [Company]
+                        , [GlExpenseCode]
+                        , [GlExpenseDescription]
+                        , [LastUpdated]
+	                    )
+                        Select  [CompanyName]
+                              , [GlExpenseCode]
+                              , [GlExpenseDescription]
+                              , @LastUpdated
+                        From    [#ResultsGlExpenseCode];
 
-	If @PrevCheck=1
-	Begin
-		Declare @CurrentCount INT, @PreviousCount INT
+                If @PrevCheck = 1
+                    Begin
+                        Declare @CurrentCount Int
+                          , @PreviousCount Int;
 	
-		Select @CurrentCount=COUNT(*) From Lookups.GlExpenseCode
-		Where LastUpdated=@LastUpdated
+                        Select  @CurrentCount = Count(*)
+                        From    [Lookups].[GlExpenseCode]
+                        Where   [LastUpdated] = @LastUpdated;
 
-		SELECT @PreviousCount=COUNT(*) From Lookups.GlExpenseCode
-		Where LastUpdated<>@LastUpdated
+                        Select  @PreviousCount = Count(*)
+                        From    [Lookups].[GlExpenseCode]
+                        Where   [LastUpdated] <> @LastUpdated;
 	
-		If @PreviousCount>@CurrentCount
-			Begin
-				Delete Lookups.GlExpenseCode
-				Where LastUpdated=@LastUpdated
-				Print 'UspUpdate_GlExpenseCode - Count has gone down since last run, no update applied'
-				Print 'Current Count = '+CAST(@CurrentCount As VARCHAR(5))+' Previous Count = '+CAST(@PreviousCount As VARCHAR(5))
-			End
-		If @PreviousCount<=@CurrentCount
-			Begin
-				Delete Lookups.GlExpenseCode
-				Where LastUpdated<>@LastUpdated
-				Print 'UspUpdate_GlExpenseCode - Update applied successfully'
-			End
-	end
-	If @PrevCheck=0
-		Begin
-			Delete Lookups.GlExpenseCode
-			Where LastUpdated<>@LastUpdated
-			Print 'UspUpdate_GlExpenseCode - Update applied successfully'
-		End
-	End
-End
-If DATEDIFF(Hour,@LastDate,GETDATE())<=@HoursBetweenUpdates
-Begin
-	Print 'UspUpdate_GlExpenseCode - Table was last updated at '+CAST(@LastDate As VARCHAR(255))+' no update applied'
-End
+                        If @PreviousCount > @CurrentCount
+                            Begin
+                                Delete  [Lookups].[GlExpenseCode]
+                                Where   [LastUpdated] = @LastUpdated;
+                                Print 'UspUpdate_GlExpenseCode - Count has gone down since last run, no update applied';
+                                Print 'Current Count = '
+                                    + Cast(@CurrentCount As Varchar(5))
+                                    + ' Previous Count = '
+                                    + Cast(@PreviousCount As Varchar(5));
+                            End;
+                        If @PreviousCount <= @CurrentCount
+                            Begin
+                                Delete  [Lookups].[GlExpenseCode]
+                                Where   [LastUpdated] <> @LastUpdated;
+                                Print 'UspUpdate_GlExpenseCode - Update applied successfully';
+                            End;
+                    End;
+                If @PrevCheck = 0
+                    Begin
+                        Delete  [Lookups].[GlExpenseCode]
+                        Where   [LastUpdated] <> @LastUpdated;
+                        Print 'UspUpdate_GlExpenseCode - Update applied successfully';
+                    End;
+            End;
+    End;
+    If DateDiff(Hour , @LastDate , GetDate()) <= @HoursBetweenUpdates
+        Begin
+            Print 'UspUpdate_GlExpenseCode - Table was last updated at '
+                + Cast(@LastDate As Varchar(255)) + ' no update applied';
+        End;
 GO
