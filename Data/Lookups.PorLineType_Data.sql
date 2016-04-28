bd12cc07ba128dab3a89e5b82fa88e8ff9c1966a
Beardy <chris.johnson85@gmail.com>
2016-04-28 14:16:21 +0100

    unlink data as set by SP

--- Data/Lookups.PorLineType_Data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO [Lookups].[PorLineType] ([PorLineType], [PorLineTypeDesc], [LastUpdated]) VALUES (1, 'Stocked line', '2016-04-01 16:27:25.8070000')
-INSERT INTO [Lookups].[PorLineType] ([PorLineType], [PorLineTypeDesc], [LastUpdated]) VALUES (4, 'Freight line', '2016-04-01 16:27:25.8070000')
-INSERT INTO [Lookups].[PorLineType] ([PorLineType], [PorLineTypeDesc], [LastUpdated]) VALUES (5, 'Other charges', '2016-04-01 16:27:25.8070000')
-INSERT INTO [Lookups].[PorLineType] ([PorLineType], [PorLineTypeDesc], [LastUpdated]) VALUES (6, 'CommentLine', '2016-04-01 16:27:25.8070000')
-INSERT INTO [Lookups].[PorLineType] ([PorLineType], [PorLineTypeDesc], [LastUpdated]) VALUES (7, 'Non-Stocked line', '2016-04-01 16:27:25.8070000')
