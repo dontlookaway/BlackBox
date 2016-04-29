14d4b0c354f9e5522eee5ac96f16991dcbacef72
Beardy <chris.johnson85@gmail.com>
2016-04-29 15:18:41 +0100

    Added results table format to ensure consistency of data types, formatted code

--- Stored Procedures/Report.UspResults_TrialBalance.sql
+++ Stored Procedures/Report.UspResults_TrialBalance.sql
@@ -20,35 +20,72 @@ Template designed by Chris Johnson, Prometic Group April 2016
             @UsedByType = @RedTagType , @UsedByName = @RedTagUse ,
             @UsedByDb = @RedTagDB;
 
-        Select  [GH].[Company]
-              , [CN].[ShortName]
-              , [CN].[CompanyName]
-              , [CN].[Currency]
-              , [GH].[GlCode]
-              , [GlDescription] = [GM].[Description]
-              , [ReportIndex1] = Case When [GM].[ReportIndex1] = '' Then Null
-                                      Else [GM].[ReportIndex1]
-                                 End
-              , [ReportIndex2] = Case When [GM].[ReportIndex2] = '' Then Null
-                                      Else [GM].[ReportIndex2]
-                                 End
-              , [GM].[GlGroup]
-              , [GH].[GlYear]
-              , [GH].[ClosingBalance]
-              , [Debit] = Case When [GH].[ClosingBalance] > 0
-                               Then [GH].[ClosingBalance]
-                               Else 0
-                          End
-              , [Credit] = Case When [GH].[ClosingBalance] < 0
-                                Then [GH].[ClosingBalance]
-                                Else 0
-                           End
-              , [Period] = Case When [GH].[Period] = 'BeginYearBalance' Then 0
-                                Else Convert(Smallint , Replace([GH].[Period] ,
+        Create Table [#Results]
+            (
+              [Company] Varchar(10)
+            , [ShortName] Varchar(150)
+            , [CompanyName] Varchar(250)
+            , [Currency] Varchar(10)
+            , [GlCode] Varchar(35)
+            , [GlDescription] Varchar(50)
+            , [ReportIndex1] Varchar(35)
+            , [ReportIndex2] Varchar(35)
+            , [GlGroup] Varchar(10)
+            , [GlYear] Int
+            , [ClosingBalance] Numeric(20 , 2)
+            , [Debit] Numeric(20 , 2)
+            , [Credit] Numeric(20 , 2)
+            , [Period] TinyInt
+            );
+
+        Insert  [#Results]
+                ( [Company]
+                , [ShortName]
+                , [CompanyName]
+                , [Currency]
+                , [GlCode]
+                , [GlDescription]
+                , [ReportIndex1]
+                , [ReportIndex2]
+                , [GlGroup]
+                , [GlYear]
+                , [ClosingBalance]
+                , [Debit]
+                , [Credit]
+                , [Period]
+                )
+                Select  [GH].[Company]
+                      , [CN].[ShortName]
+                      , [CN].[CompanyName]
+                      , [CN].[Currency]
+                      , [GH].[GlCode]
+                      , [GlDescription] = [GM].[Description]
+                      , [ReportIndex1] = Case When [GM].[ReportIndex1] = ''
+                                              Then Null
+                                              Else [GM].[ReportIndex1]
+                                         End
+                      , [ReportIndex2] = Case When [GM].[ReportIndex2] = ''
+                                              Then Null
+                                              Else [GM].[ReportIndex2]
+                                         End
+                      , [GM].[GlGroup]
+                      , [GH].[GlYear]
+                      , [GH].[ClosingBalance]
+                      , [Debit] = Case When [GH].[ClosingBalance] > 0
+                                       Then [GH].[ClosingBalance]
+                                       Else 0
+                                  End
+                      , [Credit] = Case When [GH].[ClosingBalance] < 0
+                                        Then [GH].[ClosingBalance]
+                                        Else 0
+                                   End
+                      , [Period] = Case When [GH].[Period] = 'BeginYearBalance'
+                                        Then 0
+                                        Else Convert(Smallint , Replace([GH].[Period] ,
                                                               'ClosingBalPer' ,
                                                               ''))
-                           End
-        From    [SysproCompany40].[dbo].[GenHistory] Unpivot ( [ClosingBalance] For [Period] In ( [BeginYearBalance] ,
+                                   End
+                From    [SysproCompany40].[dbo].[GenHistory] Unpivot ( [ClosingBalance] For [Period] In ( [BeginYearBalance] ,
                                                               [ClosingBalPer1] ,
                                                               [ClosingBalPer2] ,
                                                               [ClosingBalPer3] ,
@@ -64,11 +101,29 @@ Template designed by Chris Johnson, Prometic Group April 2016
                                                               [ClosingBalPer13] ,
                                                               [ClosingBalPer14] ,
                                                               [ClosingBalPer15] ) ) As [GH]
-                Left Join [SysproCompany40].[dbo].[GenMaster] [GM]
-                    On [GM].[Company] = [GH].[Company]
-                       And [GM].[GlCode] = [GH].[GlCode]
-                Left Join [BlackBox].[Lookups].[CompanyNames] [CN]
-                    On [CN].[Company] = [GH].[Company]
-        Where   [GH].[GlCode] Not In ( 'RETAINED' , 'FORCED' );
+                        Left Join [SysproCompany40].[dbo].[GenMaster] [GM]
+                            On [GM].[Company] = [GH].[Company]
+                               And [GM].[GlCode] = [GH].[GlCode]
+                        Left Join [BlackBox].[Lookups].[CompanyNames] [CN]
+                            On [CN].[Company] = [GH].[Company]
+                Where   [GH].[GlCode] Not In ( 'RETAINED' , 'FORCED' );
+
+        Select  [R].[Company]
+              , [R].[ShortName]
+              , [R].[CompanyName]
+              , [R].[Currency]
+              , [R].[GlCode]
+              , [R].[GlDescription]
+              , [R].[ReportIndex1]
+              , [R].[ReportIndex2]
+              , [R].[GlGroup]
+              , [R].[GlYear]
+              , [R].[ClosingBalance]
+              , [R].[Debit]
+              , [R].[Credit]
+              , [R].[Period]
+        From    [#Results] [R];
+
+        Drop Table [#Results];
     End;
 GO
