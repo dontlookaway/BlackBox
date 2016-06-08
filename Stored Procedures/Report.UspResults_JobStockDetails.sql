c1a1c77843f69fb597d344806703204a26f6b6f9
Beardy <chris.johnson85@gmail.com>
2016-06-08 10:21:40 +0100

    removed extraneous, changed nocount to new standard, removed leading zeros from lot numbers

--- Stored Procedures/Report.UspResults_JobStockDetails.sql
+++ Stored Procedures/Report.UspResults_JobStockDetails.sql
@@ -1,4 +1,3 @@
-
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -9,25 +8,23 @@ CREATE Proc [Report].[UspResults_JobStockDetails]
     , @RedTagType Char(1)
     , @RedTagUse Varchar(500)
     )
-As --exec Report.UspResults_JobStockDetails @Company=10, @RedTagType='M', @RedTagUse='Testing'
-
+As
     Begin
-/*
-Template designed by Chris Johnson, Prometic Group September 2015
-Stored procedure set out to query multiple databases with the same information and return it in a collated format
-*/
+--remove nocount on to speed up query
+        Set NoCount On;
+
         If IsNumeric(@Company) = 0
             Begin
                 Select  @Company = Upper(@Company);
             End;
---remove nocount on to speed up query
-        Set NoCount On;
+
 	--Red tag
-    Declare @RedTagDB Varchar(255)= Db_Name();
-    Exec [Process].[UspInsert_RedTagLogs] @StoredProcDb = 'BlackBox' ,
-        @StoredProcSchema = 'Report' , @StoredProcName = 'UspResults_JobStockDetails' ,
-        @UsedByType = @RedTagType , @UsedByName = @RedTagUse ,
-        @UsedByDb = @RedTagDB;
+        Declare @RedTagDB Varchar(255)= Db_Name();
+        Exec [Process].[UspInsert_RedTagLogs] @StoredProcDb = 'BlackBox' ,
+            @StoredProcSchema = 'Report' ,
+            @StoredProcName = 'UspResults_JobStockDetails' ,
+            @UsedByType = @RedTagType , @UsedByName = @RedTagUse ,
+            @UsedByDb = @RedTagDB;
 
 --list the tables that are to be pulled back from each DB - if they are not found the script will not be run against that db
         Declare @ListOfTables Varchar(Max) = 'InvMovements,InvMaster'; 
@@ -46,7 +43,6 @@ Stored procedure set out to query multiple databases with the same information a
             , [TrnValue] Float
             , [UnitCost] Numeric(20 , 7)
             );
-
         Create Table [#InvMaster]
             (
               [DatabaseName] Varchar(150)
@@ -62,23 +58,16 @@ Stored procedure set out to query multiple databases with the same information a
 	Declare @DB varchar(150),@DBCode varchar(150)
 	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end
 	IF left(@DB,13)=''SysproCompany'' and right(@DB,3)<>''SRS''
-	BEGIN'
-            + --only companies selected in main run, or if companies selected then all
-            '
+	BEGIN
 		IF @DBCode in (''' + Replace(@Company , ',' , ''',''') + ''') or '''
             + Upper(@Company) + ''' = ''ALL''
-			Declare @ListOfTables VARCHAR(max) = ''' + @ListOfTables + '''
+			Declare @ListOfTables VARCHAR(max) = ''' + @ListOfTables
+            + '''
 					, @RequiredCountOfTables INT
-					, @ActualCountOfTables INT'
-            + --count number of tables requested (number of commas plus one)
-            '
-			Select @RequiredCountOfTables= count(1) from  BlackBox.dbo.[udf_SplitString](@ListOfTables,'','')'
-            + --Count of the tables requested how many exist in the db
-            '
+					, @ActualCountOfTables INT
+			Select @RequiredCountOfTables= count(1) from  BlackBox.dbo.[udf_SplitString](@ListOfTables,'','')
 			Select @ActualCountOfTables = COUNT(1) FROM sys.tables
-			Where name In (Select Value Collate Latin1_General_BIN From BlackBox.dbo.udf_SplitString(@ListOfTables,'','')) '
-            + --only if the count matches (all the tables exist in the requested db) then run the script
-            '
+			Where name In (Select Value Collate Latin1_General_BIN From BlackBox.dbo.udf_SplitString(@ListOfTables,'','')) 
 			If @ActualCountOfTables=@RequiredCountOfTables
 			BEGIN
 				    Insert  #InvMovements
@@ -114,23 +103,16 @@ Stored procedure set out to query multiple databases with the same information a
 	Declare @DB varchar(150),@DBCode varchar(150)
 	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end
 	IF left(@DB,13)=''SysproCompany'' and right(@DB,3)<>''SRS''
-	BEGIN'
-            + --only companies selected in main run, or if companies selected then all
-            '
+	BEGIN
 		IF @DBCode in (''' + Replace(@Company , ',' , ''',''') + ''') or '''
             + Upper(@Company) + ''' = ''ALL''
-			Declare @ListOfTables VARCHAR(max) = ''' + @ListOfTables + '''
+			Declare @ListOfTables VARCHAR(max) = ''' + @ListOfTables
+            + '''
 					, @RequiredCountOfTables INT
-					, @ActualCountOfTables INT'
-            + --count number of tables requested (number of commas plus one)
-            '
-			Select @RequiredCountOfTables= count(1) from  BlackBox.dbo.[udf_SplitString](@ListOfTables,'','')'
-            + --Count of the tables requested how many exist in the db
-            '
+					, @ActualCountOfTables INT
+			Select @RequiredCountOfTables= count(1) from  BlackBox.dbo.[udf_SplitString](@ListOfTables,'','')
 			Select @ActualCountOfTables = COUNT(1) FROM sys.tables
-			Where name In (Select Value Collate Latin1_General_BIN From BlackBox.dbo.udf_SplitString(@ListOfTables,'','')) '
-            + --only if the count matches (all the tables exist in the requested db) then run the script
-            '
+			Where name In (Select Value Collate Latin1_General_BIN From BlackBox.dbo.udf_SplitString(@ListOfTables,'','')) 
 			If @ActualCountOfTables=@RequiredCountOfTables
 			BEGIN
 				Insert #InvMaster
@@ -146,18 +128,16 @@ Stored procedure set out to query multiple databases with the same information a
 			FROM InvMaster
 			End
 	End';
---Enable this function to check script changes (try to run script directly against db manually)
---Print @SQL
 
 --execute script against each db, populating the base tables
         Exec [Process].[ExecForEachDB] @cmd = @SQL1;
         Exec [Process].[ExecForEachDB] @cmd = @SQL2;
 
---define the results you want to return
 
 --Placeholder to create indexes as required
 
 --return results
+        Set NoCount Off;
         Select  [IM].[DatabaseName]
               , [IM].[Job]
               , [IM].[Warehouse]
@@ -165,12 +145,18 @@ Stored procedure set out to query multiple databases with the same information a
               , [IM].[StockCode]
               , [IMA].[StockDescription]
               , [OutwardLot] = Case When [IM].[TrnType] = 'R'
-                                    Then [IM].[LotSerial]
+                                    Then Substring([IM].[LotSerial] ,
+                                                   PatIndex('%[^0]%' ,
+                                                            [IM].[LotSerial]) ,
+                                                   10)
                                     Else Null
                                End --receipted Lots generated from job
               , [InwardLot] = Case When [IM].[TrnType] <> 'R'
                                         And [IM].[LotSerial] <> ''
-                                   Then [IM].[LotSerial]
+                                   Then Substring([IM].[LotSerial] ,
+                                                  PatIndex('%[^0]%' ,
+                                                           [IM].[LotSerial]) ,
+                                                  10)
                                    Else Null
                               End --receipted Lots generated from job
               , [IM].[TrnType]
@@ -179,23 +165,29 @@ Stored procedure set out to query multiple databases with the same information a
               , [IMA].[StockUom]
               , [IM].[UnitCost]
         From    [#InvMovements] [IM]
-                Left Join [BlackBox].[Lookups].[TrnTypeAmountModifier] [TT] On [TT].[TrnType] = [IM].[TrnType] Collate Latin1_General_BIN
-                                                              And [TT].[Company] = [IM].[DatabaseName] Collate Latin1_General_BIN
-                Left Join [#InvMaster] [IMA] On [IMA].[StockCode] = [IM].[StockCode] Collate Latin1_General_BIN
-                                                And [IMA].[DatabaseName] = [IM].[DatabaseName] Collate Latin1_General_BIN
+                Left Join [BlackBox].[Lookups].[TrnTypeAmountModifier] [TT]
+                    On [TT].[TrnType] = [IM].[TrnType] Collate Latin1_General_BIN
+                       And [TT].[Company] = [IM].[DatabaseName] Collate Latin1_General_BIN
+                Left Join [#InvMaster] [IMA]
+                    On [IMA].[StockCode] = [IM].[StockCode] Collate Latin1_General_BIN
+                       And [IMA].[DatabaseName] = [IM].[DatabaseName] Collate Latin1_General_BIN
         Group By [IM].[DatabaseName]
               , [IM].[Job]
               , [IM].[Warehouse]
               , [IM].[Bin]
               , [IM].[StockCode]
               , [IMA].[StockDescription]
-              , Case When [IM].[TrnType] = 'R' Then [IM].[LotSerial]
+              , Case When [IM].[TrnType] = 'R'
+                     Then Substring([IM].[LotSerial] ,
+                                    PatIndex('%[^0]%' , [IM].[LotSerial]) , 10)
                      Else Null
-                End
+                End 
               , Case When [IM].[TrnType] <> 'R'
-                          And [IM].[LotSerial] <> '' Then [IM].[LotSerial]
+                          And [IM].[LotSerial] <> ''
+                     Then Substring([IM].[LotSerial] ,
+                                    PatIndex('%[^0]%' , [IM].[LotSerial]) , 10)
                      Else Null
-                End
+                End 
               , [IM].[TrnType]
               , [IMA].[StockUom]
               , [IM].[UnitCost];
