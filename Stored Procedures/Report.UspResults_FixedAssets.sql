26c03bcaff09a826f5e4b153ee8211fa524b444d
Beardy <chris.johnson85@gmail.com>
2016-07-15 10:44:37 +0100

    Corrected NoCount, added disposal flag and reasons, changed to exec with table check, formatted proc

--- Stored Procedures/Report.UspResults_FixedAssets.sql
+++ Stored Procedures/Report.UspResults_FixedAssets.sql
@@ -1,4 +1,3 @@
-
 SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
@@ -16,14 +15,14 @@ Exec [Report].[UspResults_FixedAssets] 10
 */
     Begin
 
-        Set NoCount Off;
+        Set NoCount On;
+
         If IsNumeric(@Company) = 0
             Begin
                 Select  @Company = Upper(@Company);
             End;
 
---remove nocount on to speed up query
-        Set NoCount On;
+
 
 --Red tag
         Declare @RedTagDB Varchar(255)= Db_Name();
@@ -52,6 +51,8 @@ Exec [Report].[UspResults_FixedAssets] 10
             , [PurchaseDate] Date
             , [FirstInstalDate] Date
             , [DateSold] Date
+            , [DisposedFlag] Char(1)
+            , [DisposalReason] Varchar(10)
             );
         Create Table [#AssetType]
             (
@@ -71,33 +72,22 @@ Exec [Report].[UspResults_FixedAssets] 10
             , [AssetGroupCode] Varchar(10)
             , [Description] Varchar(100)
             );
+        Create Table [#AssetReasonDisp]
+            (
+              [DatabaseName] Varchar(150)
+            , [DisposalReason] Varchar(10)
+            , [Description] Varchar(50)
+            );
 
 --create script to pull data from each db into the tables
         Declare @SQL1 Varchar(Max) = '
 	USE [?];
 	Declare @DB varchar(150),@DBCode varchar(150)
-	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end'
-            + --Only query DBs beginning SysProCompany
-            '
+	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end
 	IF left(@DB,13)=''SysproCompany'' and right(@DB,3)<>''SRS''
-	BEGIN'
-            + --only companies selected in main run, or if companies selected then all
-            '
+	BEGIN
 		IF @DBCode in (''' + Replace(@Company , ',' , ''',''') + ''') or '''
             + Upper(@Company) + ''' = ''ALL''
-			Declare @ListOfTables VARCHAR(max) = ''' + @ListOfTables + '''
-					, @RequiredCountOfTables INT
-					, @ActualCountOfTables INT'
-            + --count number of tables requested (number of commas plus one)
-            '
-			Select @RequiredCountOfTables= count(1) from  BlackBox.dbo.[udf_SplitString](@ListOfTables,'','')'
-            + --Count of the tables requested how many exist in the db
-            '
-			Select @ActualCountOfTables = COUNT(1) FROM sys.tables
-			Where name In (Select Value Collate Latin1_General_BIN From BlackBox.dbo.udf_SplitString(@ListOfTables,'','')) '
-            + --only if the count matches (all the tables exist in the requested db) then run the script
-            '
-			If @ActualCountOfTables=@RequiredCountOfTables
 			BEGIN
 				Insert [#AssetMaster]
 				( [DatabaseName]
@@ -109,10 +99,12 @@ Exec [Report].[UspResults_FixedAssets] 10
 				, [GlCode]
 				, [AssetType]
 				, [Location]
-				,[AssetGroupCode]
-				, PurchaseDate		
-				, FirstInstalDate 
-				, DateSold	
+				, [AssetGroupCode]
+				, [PurchaseDate]		
+				, [FirstInstalDate]
+				, [DateSold]
+				, [DisposedFlag] 
+				, [DisposalReason] 
 				)
 				SELECT [DatabaseName] = @DBCode
 				, [am].[Asset]
@@ -127,107 +119,59 @@ Exec [Report].[UspResults_FixedAssets] 10
 				, [am].PurchaseDate		
 				, [am].FirstInstalDate 
 				, [am].DateSold
+				, [am].[DisposedFlag] 
+				, [am].[DisposalReason] 
 				From [AssetMaster] As [am]
 			End
 	End';
         Declare @SQL2 Varchar(Max) = '
 	USE [?];
 	Declare @DB varchar(150),@DBCode varchar(150)
-	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end'
-            + --Only query DBs beginning SysProCompany
-            '
+	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end
 	IF left(@DB,13)=''SysproCompany'' and right(@DB,3)<>''SRS''
-	BEGIN'
-            + --only companies selected in main run, or if companies selected then all
-            '
+	BEGIN
 		IF @DBCode in (''' + Replace(@Company , ',' , ''',''') + ''') or '''
             + Upper(@Company) + ''' = ''ALL''
-			Declare @ListOfTables VARCHAR(max) = ''' + @ListOfTables + '''
-					, @RequiredCountOfTables INT
-					, @ActualCountOfTables INT'
-            + --count number of tables requested (number of commas plus one)
-            '
-			Select @RequiredCountOfTables= count(1) from  BlackBox.dbo.[udf_SplitString](@ListOfTables,'','')'
-            + --Count of the tables requested how many exist in the db
-            '
-			Select @ActualCountOfTables = COUNT(1) FROM sys.tables
-			Where name In (Select Value Collate Latin1_General_BIN From BlackBox.dbo.udf_SplitString(@ListOfTables,'','')) '
-            + --only if the count matches (all the tables exist in the requested db) then run the script
-            '
-			If @ActualCountOfTables=@RequiredCountOfTables
 			BEGIN
-						Insert [#AssetType]
-		        ( [DatabaseName]
-		        , [AssetType]
-		        , [Description]
-		        )
-		SELECT [DatabaseName]=@DBCode
-             , [at].[AssetType]
-             , [at].[Description] FROM [AssetType] As [at]
+				Insert [#AssetType]
+					( [DatabaseName]
+					, [AssetType]
+					, [Description]
+					)
+				SELECT [DatabaseName]=@DBCode
+					 , [at].[AssetType]
+					 , [at].[Description] 
+				FROM [AssetType] As [at]
 			End
 	End';
         Declare @SQL3 Varchar(Max) = '
 	USE [?];
 	Declare @DB varchar(150),@DBCode varchar(150)
-	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end'
-            + --Only query DBs beginning SysProCompany
-            '
+	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end
 	IF left(@DB,13)=''SysproCompany'' and right(@DB,3)<>''SRS''
-	BEGIN'
-            + --only companies selected in main run, or if companies selected then all
-            '
+	BEGIN
 		IF @DBCode in (''' + Replace(@Company , ',' , ''',''') + ''') or '''
             + Upper(@Company) + ''' = ''ALL''
-			Declare @ListOfTables VARCHAR(max) = ''' + @ListOfTables + '''
-					, @RequiredCountOfTables INT
-					, @ActualCountOfTables INT'
-            + --count number of tables requested (number of commas plus one)
-            '
-			Select @RequiredCountOfTables= count(1) from  BlackBox.dbo.[udf_SplitString](@ListOfTables,'','')'
-            + --Count of the tables requested how many exist in the db
-            '
-			Select @ActualCountOfTables = COUNT(1) FROM sys.tables
-			Where name In (Select Value Collate Latin1_General_BIN From BlackBox.dbo.udf_SplitString(@ListOfTables,'','')) '
-            + --only if the count matches (all the tables exist in the requested db) then run the script
-            '
-			If @ActualCountOfTables=@RequiredCountOfTables
 			BEGIN
-						Insert [#AssetLocation]
+				Insert [#AssetLocation]
 		        ( [DatabaseName]
 		        , [Location]
 		        , [Description]
 		        )
-		SELECT [DatabaseName]=@DBCode
-             , [al].[Location]
-             , [al].[Description] 
-		FROM [AssetLocation] As [al]
+				SELECT [DatabaseName]=@DBCode
+					 , [al].[Location]
+					 , [al].[Description] 
+				FROM [AssetLocation] As [al]
 			End
 	End';
         Declare @SQL4 Varchar(Max) = '
 	USE [?];
 	Declare @DB varchar(150),@DBCode varchar(150)
-	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end'
-            + --Only query DBs beginning SysProCompany
-            '
+	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end
 	IF left(@DB,13)=''SysproCompany'' and right(@DB,3)<>''SRS''
-	BEGIN'
-            + --only companies selected in main run, or if companies selected then all
-            '
+	BEGIN
 		IF @DBCode in (''' + Replace(@Company , ',' , ''',''') + ''') or '''
             + Upper(@Company) + ''' = ''ALL''
-			Declare @ListOfTables VARCHAR(max) = ''' + @ListOfTables + '''
-					, @RequiredCountOfTables INT
-					, @ActualCountOfTables INT'
-            + --count number of tables requested (number of commas plus one)
-            '
-			Select @RequiredCountOfTables= count(1) from  BlackBox.dbo.[udf_SplitString](@ListOfTables,'','')'
-            + --Count of the tables requested how many exist in the db
-            '
-			Select @ActualCountOfTables = COUNT(1) FROM sys.tables
-			Where name In (Select Value Collate Latin1_General_BIN From BlackBox.dbo.udf_SplitString(@ListOfTables,'','')) '
-            + --only if the count matches (all the tables exist in the requested db) then run the script
-            '
-			If @ActualCountOfTables=@RequiredCountOfTables
 			BEGIN
 			Insert [#AssetGroup]
 	        ( [DatabaseName]
@@ -240,19 +184,41 @@ Exec [Report].[UspResults_FixedAssets] 10
 			 FROM [dbo].[AssetGroup] As [ag]
 			End
 	End';
+        Declare @SQL5 Varchar(Max) = '
+	USE [?];
+	Declare @DB varchar(150),@DBCode varchar(150)
+	Select @DB = DB_NAME(),@DBCode = case when len(db_Name())>13 then right(db_Name(),len(db_Name())-13) else null end
+	IF left(@DB,13)=''SysproCompany'' and right(@DB,3)<>''SRS''
+	BEGIN
+		IF @DBCode in (''' + Replace(@Company , ',' , ''',''') + ''') or '''
+            + Upper(@Company) + ''' = ''ALL''
+			BEGIN
+			Insert  [#AssetReasonDisp]
+                ( [DatabaseName]
+                , [DisposalReason]
+                , [Description]
+                )
+                Select  @DBCode
+                      , [ARD].[DisposalReason]
+                      , [ARD].[Description]
+                From    [AssetReasonDisp] [ARD];
+			End
+	End';
 --Enable this function to check script changes (try to run script directly against db manually)
 --Print @SQL
 
 --execute script against each db, populating the base tables
-        Print 1;
-        Exec [Process].[ExecForEachDB] @cmd = @SQL1;
-        Print 2;
-        Exec [Process].[ExecForEachDB] @cmd = @SQL2;
-        Print 3;
-        Exec [Process].[ExecForEachDB] @cmd = @SQL3;
-        Print 4;
-        Exec [Process].[ExecForEachDB] @cmd = @SQL4;
-        Print 5;
+        Exec [Process].[ExecForEachDB_WithTableCheck] @cmd = @SQL1 ,
+            @SchemaTablesToCheck = @ListOfTables;
+        Exec [Process].[ExecForEachDB_WithTableCheck] @cmd = @SQL2 ,
+            @SchemaTablesToCheck = @ListOfTables;
+        Exec [Process].[ExecForEachDB_WithTableCheck] @cmd = @SQL3 ,
+            @SchemaTablesToCheck = @ListOfTables;
+        Exec [Process].[ExecForEachDB_WithTableCheck] @cmd = @SQL4 ,
+            @SchemaTablesToCheck = @ListOfTables;
+        Exec [Process].[ExecForEachDB_WithTableCheck] @cmd = @SQL5 ,
+            @SchemaTablesToCheck = @ListOfTables;
+
 --define the results you want to return
         Create Table [#Results]
             (
@@ -269,6 +235,8 @@ Exec [Report].[UspResults_FixedAssets] 10
             , [PurchaseDate] Date
             , [FirstInstalDate] Date
             , [DateSold] Date
+            , [DisposedFlag] Char(1)
+            , [DisposalReason] Varchar(50)
             );
 
 --Placeholder to create indexes as required
@@ -288,7 +256,9 @@ Exec [Report].[UspResults_FixedAssets] 10
                 , [AssetGroup]
                 , [PurchaseDate]
                 , [FirstInstalDate]
-                , [DateSold]			
+                , [DateSold]
+                , [DisposedFlag]
+                , [DisposalReason]		
 	            )
                 Select  [am].[DatabaseName]
                       , [am].[Asset]
@@ -303,14 +273,25 @@ Exec [Report].[UspResults_FixedAssets] 10
                       , [am].[PurchaseDate]
                       , [am].[FirstInstalDate]
                       , [am].[DateSold]
+                      , [DisposedFlag] = Case When [am].[DisposedFlag] = ''
+                                              Then Null
+                                              Else [am].[DisposedFlag]
+                                         End
+                      , [ARD].[Description]
                 From    [#AssetMaster] As [am]
-                        Left Join [#AssetType] As [at] On [at].[AssetType] = [am].[AssetType]
-                                                          And [at].[DatabaseName] = [am].[DatabaseName]
-                        Left Join [#AssetLocation] As [al] On [al].[Location] = [am].[Location]
-                                                              And [al].[DatabaseName] = [am].[DatabaseName]
-                        Left Join [#AssetGroup] As [ag] On [ag].[AssetGroupCode] = [am].[AssetGroupCode]
-                                                           And [ag].[DatabaseName] = [am].[DatabaseName];
+                        Left Join [#AssetType] As [at]
+                            On [at].[AssetType] = [am].[AssetType]
+                               And [at].[DatabaseName] = [am].[DatabaseName]
+                        Left Join [#AssetLocation] As [al]
+                            On [al].[Location] = [am].[Location]
+                               And [al].[DatabaseName] = [am].[DatabaseName]
+                        Left Join [#AssetGroup] As [ag]
+                            On [ag].[AssetGroupCode] = [am].[AssetGroupCode]
+                               And [ag].[DatabaseName] = [am].[DatabaseName]
+                        Left Join [#AssetReasonDisp] [ARD]
+                            On [ARD].[DisposalReason] = [am].[DisposalReason];
 
+        Set NoCount Off;
 --return results
         Select  [cn].[CompanyName]
               , [r].[Asset]
@@ -325,8 +306,11 @@ Exec [Report].[UspResults_FixedAssets] 10
               , [r].[PurchaseDate]
               , [r].[FirstInstalDate]
               , [r].[DateSold]
+              , [r].[DisposedFlag]
+              , [r].[DisposalReason]
         From    [#Results] [r]
-                Left Join [Lookups].[CompanyNames] As [cn] On [r].[DatabaseName] = [cn].[Company];
+                Left Join [Lookups].[CompanyNames] As [cn]
+                    On [r].[DatabaseName] = [cn].[Company];
 
     End;
 
