de75ac7028e2d3b316e4ec6436c15c0f9331bc0b
Beardy <chris.johnson85@gmail.com>
2016-07-22 15:03:56 +0100

    corrected Primary key

--- Tables/Lookups.PorLineType.sql
+++ Tables/Lookups.PorLineType.sql
@@ -2,8 +2,8 @@ CREATE TABLE [Lookups].[PorLineType]
 (
 [PorLineType] [int] NOT NULL,
 [PorLineTypeDesc] [varchar] (150) COLLATE Latin1_General_BIN NULL,
-[LastUpdated] [datetime2] NULL
+[LastUpdated] [datetime2] NOT NULL
 ) ON [PRIMARY]
-ALTER TABLE [Lookups].[PorLineType] ADD 
-CONSTRAINT [PK__PorLineT__411F3B22B27B447E] PRIMARY KEY CLUSTERED  ([PorLineType]) ON [PRIMARY]
+GO
+ALTER TABLE [Lookups].[PorLineType] ADD CONSTRAINT [PK__PorLineT__67453B017BDF3668] PRIMARY KEY CLUSTERED  ([PorLineType], [LastUpdated] DESC) ON [PRIMARY]
 GO
