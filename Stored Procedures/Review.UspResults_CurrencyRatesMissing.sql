4855e91a2e787fb9521422544274f9b517cd4f9d
Beardy <chris.johnson85@gmail.com>
2016-07-26 15:45:54 +0100

    Added alternative status

--- Stored Procedures/Review.UspResults_CurrencyRatesMissing.sql
+++ Stored Procedures/Review.UspResults_CurrencyRatesMissing.sql
@@ -2,7 +2,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-Create Proc [Review].[UspResults_CurrencyRatesMissing]
+CREATE Proc [Review].[UspResults_CurrencyRatesMissing]
     (
       @RedTagType Char(1)
     , @RedTagUse Varchar(500)
@@ -27,6 +27,12 @@ As
             Begin
                 Select  [CurrencyStatus] = 'No current exchange rates';
             End;
-
+        If Exists ( Select  1
+                    From    [Lookups].[CurrencyRates] [CR]
+                    Where   GetDate() Between [CR].[StartDateTime]
+                                      And     [CR].[EndDateTime] )
+            Begin
+                Select  [CurrencyStatus] = 'Current exchange rates';
+            End;
     End;
 GO
